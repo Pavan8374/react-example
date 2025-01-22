0dd4874511383ce33ea3020194166ab9a7f31ae5
Pavan8374 <rpavank2002@gail.com>
2025-01-22 11:40:17 +0530

    Resolve auto adjust pages layout and added github actions.

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,9 +1,16 @@
-// src/components/Layout.tsx
 import { Box, CssBaseline } from '@mui/material';
 import Sidebar from './Sidebar.tsx';
 import React from 'react';
+import { useSidebar } from '../context/SidebarContext.tsx';
+import { useTheme } from '@mui/material';
+
+const drawerWidth = 240;
+const closedDrawerWidth = 65;
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
+  const { isOpen } = useSidebar();
+  const theme = useTheme();
+
   return (
     <Box sx={{ display: 'flex' }}>
       <CssBaseline />
@@ -13,8 +20,16 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
         sx={{
           flexGrow: 1,
           p: 3,
-          width: { sm: `calc(100% - ${240}px)` },
-          ml: { sm: `${240}px` },
+          width: { 
+            sm: `calc(100% - ${isOpen ? drawerWidth : closedDrawerWidth}px)` 
+          },
+          ml: { 
+            sm: `${isOpen ? drawerWidth : closedDrawerWidth}px` 
+          },
+          transition: theme.transitions.create(['width', 'margin'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen,
+          }),
         }}
       >
         {children}
